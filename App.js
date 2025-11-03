e18ad51fa7adabafc75e4649c735fe47e76d8bcf
fagnaniklarna <32934614+fagnaniklarna@users.noreply.github.com>
2025-11-03 09:41:26 -0500

    Add React frontend starter (App.js)

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the React Frontend!</h1>
+      <p>Your Express backend is ready. Connect your frontend by adding API calls here.</p>
+    </div>
+  );
+}
+
+export default App;
